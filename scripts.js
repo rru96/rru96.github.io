717541715d29d4b45dfa7f0600076ed8632fb16c
rru96 <R.Russell@gmx.com>
2021-10-31 07:18:07 -0500

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,27 @@
+let API_KEY = "a99f0f1f";
+let URL = "http://www.omdbapi.com/?apikey="+ API_KEY+"&";
+
+let movie_list = {
+    "Movies" : [
+        {
+            "title" : "The Shawshank Redemption",
+            "imdb" : "tt0111161",
+            "why" : ""
+        },
+        {
+            "title" : "The Dark Knight",
+            "imdb" : "tt0468569",
+            "why" : ""
+        },
+        {
+            "title" : "12 Angry Men",
+            "imdb" : "",
+            "why" : ""
+        },
+        {
+            "title" : "Fight Club",
+            "imdb" : "",
+            "why" : ""
+        }
+    ]
+}
